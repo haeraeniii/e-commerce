61f2eb7fa6797b140cb4d02a6bcef091162c78d6
haeraenii <jhr@learninvest.co.kr>
2024-07-19 09:10:29 +0900

    Exception 설정 & interceptor setting

--- src/main/java/hhplus/e_commerce/config/ApiAdvice.java
+++ src/main/java/hhplus/e_commerce/config/ApiAdvice.java
@@ -5,4 +5,23 @@ import hhplus.e_commerce.base.exception.CustomException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
+public class ApiAdvice {
 
+    @ExceptionHandler(CustomException.class)
+    public ApiResult customException (CustomException e) {
+
+        String message = "실패했습니다.";
+
+        switch (e.getExceptionType()) {
+
+            case STOCK_SHORTAGE :
+                message = "재고가 부족합니다.";
+                break;
+            case BALANCE_SHORTAGE:
+                message = "잔액이 부족합니다.";
+        }
+
+        return new ApiResult(false, message);
+    }
+}
--- src/main/java/hhplus/e_commerce/config/WebMvcConfig.java
+++ src/main/java/hhplus/e_commerce/config/WebMvcConfig.java
@@ -5,3 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/images/**", "/js/**");
+    }
+}
